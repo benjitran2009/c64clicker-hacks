3176cc5f0f0ad88d7e834cac7948f3744a450f50
benjitran2009 <56853870+benjitran2009@users.noreply.github.com>
2022-12-06 16:12:54 -0600

    Create cps.js

---
+++ cps.js
@@ -0,0 +1,2 @@
+/* Developed by benjitran2009 on https://github.com/benjitran2009/c64clicker-hacks/ */
+function ox4312() {console.log("Developed Ben, loading...")};function ox5516(args) {window.CClicker.cps._d=[args]};function ox9413() {var e=prompt("Amount");return e} var t=ox9413();ox5516(t);ox4312();alert("finished! click a upgrade to continue.")
